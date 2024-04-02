201e92b32fffa9e7aa0e34bd9a4b73a91751f083
Pedro Sa <pedro.sa@loka.com>
2024-04-02 10:41:46 +0100

    iinitial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Terraform
+gcloud auth application-default login
+export GOOGLE_CREDENTIALS=path to gcp cred json file
---
+++ terraform/main.tf
@@ -0,0 +1,59 @@
+
+variable "GCP_BUCKET" {
+}
+
+variable "GCP_BUCKET_LOCATION" {
+}
+
+variable "GCP_REGION" {
+}
+
+variable "GCP_PROJECT" {
+}
+
+
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+
+provider "google" {
+# Credentials only needs to be set if you do not have the GOOGLE_APPLICATION_CREDENTIALS set
+#  credentials = 
+  project = var.GCP_PROJECT
+  region  = var.GCP_REGION
+}
+
+
+
+resource "google_storage_bucket" "data-lake-bucket" {
+  name          = var.GCP_BUCKET
+  location      = var.GCP_BUCKET_LOCATION
+
+  # Optional, but recommended settings:
+  storage_class = "STANDARD"
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled     = true
+  }
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 30  // days
+    }
+  }
+
+  force_destroy = true
+}
+
+
