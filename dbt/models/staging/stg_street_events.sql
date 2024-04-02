03f0188877e7efaf44ea5e98e179030111131c5c
Pedro Sá <pedro.sousa.sa@gmail.com>
2024-04-02 17:26:40 +0000

    added stg street events table and stg schema

--- dbt/models/staging/stg_street_events.sql
+++ dbt/models/staging/stg_street_events.sql
@@ -1,11 +1,20 @@
-select
-    dt_registo as event_date,	
-    area as event_category, 
-    tipo as event_type,
-    Freguesia as municipal_parish,
-    Subseccao as municipal_subsection,
-    Longitude_Subseccao as lon_subsection,
-    Latitude_Subseccao as lat_subsection
+with source as (
+    select * from {{source('staging', 'external_street_events')}}
+),
+
+renamed as (
+    select
+        dt_registo as event_date,	
+        area as event_category, 
+        tipo as event_type,
+        Freguesia as municipal_parish,
+        Subseccao as municipal_subsection,
+        Longitude_Subseccao as lon_subsection,
+        Latitude_Subseccao as lat_subsection
+    
+    from source
+)
+
+select * from renamed
 
 
-from {{ source('staging', 'street_events') }}
